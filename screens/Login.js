496cf276c169317c3c780893cc14ee18b176caf8
waheednazeer <waheed.nazeer@hotmail.com>
2024-02-06 14:17:32 +0500

    Button 1 & Button 2 added on Login page

--- screens/Login.js
+++ screens/Login.js
@@ -56,7 +56,7 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
             <TouchableOpacity
                style={styles.sendButton}
               onPress={async ()=>{
-                console.log(phoneNumber+' Button was pressed')
+                console.log(phoneNumber+' Hello Waheed')
     
                 const sendTextResponse=await fetch(
                   'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
@@ -75,7 +75,32 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
                 }
               }}
             >
-              <Text style={{color:'white'}}>Send</Text>      
+              <Text style={{color:'white'}}>Button 1</Text>      
+            </TouchableOpacity>
+
+            <TouchableOpacity
+               style={styles.sendButton}
+              onPress={async ()=>{
+                console.log(phoneNumber+' Hello Waheed')
+    
+                const sendTextResponse=await fetch(
+                  'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+                  {
+                    method:'POST',
+                    headers:{
+                     'content-type':'application/text'
+                   }
+                  }
+                )
+                const sendTextResponseData = await sendTextResponse.text();
+                if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
+                  await Alert.alert("Did you type your number correctly? "+phoneNumber);
+                } else{
+                  setLoggedInState(loggedInStates.LOGGING_IN);
+                }
+              }}
+            >
+              <Text style={{color:'white'}}>Button 2</Text>      
             </TouchableOpacity>
     
           </View>
