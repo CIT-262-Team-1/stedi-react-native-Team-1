016fed722dea486d8f4455051bba177de35c087e
mcleanlu <lucas.w.mclean@gmail.com>
2024-02-07 20:16:45 -0700

    Luke Merge for Button

--- screens/Login.js
+++ screens/Login.js
@@ -106,6 +106,31 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
             <TouchableOpacity
                style={styles.sendButton}
               onPress={async ()=>{
+                console.log(phoneNumber+' Hello Luke')
+    
+                const sendTextResponse=await fetch(
+                  'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+                  {
+                    method:'POST',
+                    headers:{
+                     'content-type':'application/text'
+                   }
+                  }
+                )
+                const sendTextResponseData = await sendTextResponse.text();
+                if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
+                  await Alert.alert("Did you type your number correctly? "+phoneNumber);
+                } else{
+                  setLoggedInState(loggedInStates.LOGGING_IN);
+                }
+              }}
+            >
+              <Text style={{color:'red'}}>Button 2</Text>      
+            </TouchableOpacity>
+
+            <TouchableOpacity
+               style={styles.sendButton}
+              onPress={async ()=>{
                 console.log(phoneNumber+' Hello Button 2')
     
                 const sendTextResponse=await fetch(
